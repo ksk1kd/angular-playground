253d3405ff1b91a13dc1924555f73aa90bd885c8
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-02 01:19:59 +0900

    change component directory structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ComponentInput } from 'components/component-input/component-input.component';
-import { ComponentModelInput } from 'components/component-model-input/component-model-input.component';
-import { ComponentOutput } from 'components/component-output/component-output.component';
-import { ControlFlow } from 'components/control-flow/control-flow.component';
-import { Forms } from 'components/forms/forms.component';
-import { NgContentPlayground } from 'components/ng-content-playground/ng-content-playground.component';
+import { ComponentInput } from './component-input/component-input.component';
+import { ComponentModelInput } from './component-model-input/component-model-input.component';
+import { ComponentOutput } from './component-output/component-output.component';
+import { ControlFlow } from './control-flow/control-flow.component';
+import { Forms } from './forms/forms.component';
+import { NgContentPlayground } from './ng-content-playground/ng-content-playground.component';
 
 @Component({
   selector: 'app-root',
