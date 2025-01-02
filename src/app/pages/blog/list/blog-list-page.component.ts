dff91b25393c8dad891264895ca3f944b79a7179
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-02 23:42:08 +0900

    add blog detail page links at blog list page

--- src/app/pages/blog/list/blog-list-page.component.ts
+++ src/app/pages/blog/list/blog-list-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'blog-list-page',
+  imports: [RouterLink],
   templateUrl: './blog-list-page.component.html',
 })
 export class BlogListPage {}
