b187781a66e3edaada6c34f0179c5ed4a7912fbb
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-02 23:46:44 +0900

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import type { Routes } from '@angular/router';
 import { BlogDetailPage } from './pages/blog/detail/blog-detail-page.component';
 import { BlogListPage } from './pages/blog/list/blog-list-page.component';
+import { NotFoundPage } from './pages/not-found/not-found-page.component';
 import { TopPage } from './pages/top/top-page.component';
 
 export const routes: Routes = [
   { path: '', component: TopPage },
   { path: 'blog', component: BlogListPage },
   { path: 'blog/:id', component: BlogDetailPage },
+  { path: '**', component: NotFoundPage },
 ];
