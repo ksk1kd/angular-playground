dd51a71c28ed6cbd60915cb61840ed09211af5ac
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-03 01:41:36 +0900

    configure hybrid rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,25 @@
-import { RenderMode, type ServerRoute } from '@angular/ssr';
+import { PrerenderFallback, RenderMode, type ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'blog/:id',
     renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.None,
+    async getPrerenderParams() {
+      const ids = ['1', '2', '3', '4', '5'];
+      return ids.map((id) => ({ id }));
+    },
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server,
   },
 ];
